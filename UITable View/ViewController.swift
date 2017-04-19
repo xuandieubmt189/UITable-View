df53ff743f464d71489e4f3e41273796565babb5
Xuan Dieu <xuandieubmt189@gmail.com>
2017-04-19 14:51:58 +0700

    new

---
+++ UITable View/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITable View
+//
+//  Created by Cntt12 on 4/19/17.
+//  Copyright © 2017 Cntt12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
